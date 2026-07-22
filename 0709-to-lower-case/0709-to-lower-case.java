a2be4ca74c67255cea875ae1ed4f45d9a5341400
Mohamed Hafis <mdhafiss123@gmail.com>
2026-07-22 22:44:23 +0530

    Time: 0 ms (100%), Space: 42.8 MB (67.6%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,5 +1,13 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        char[] chars = s.toCharArray();
+        
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] >= 'A' && chars[i] <= 'Z') {
+                chars[i] = (char) (chars[i] + ('a' - 'A'));
+            }
+        }
+        
+        return new String(chars);
     }
 }
